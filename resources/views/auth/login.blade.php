8234326e6b29e1483f5fe893577c8eb7f94b8393
UzielMendez <umendezlucio@gmail.com>
2023-02-07 08:19:14 -0500

    First Commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.navbars.guest.topnav', [
+        'classes' => 'top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent mt-4',
+        'container' => 'container',
+        'text' => 'text-white',
+        'arrow' => '/assets/img/down-arrow-white.svg',
+    ])
+    <main class="main-content  mt-0">
+        <div class="page-header align-items-start min-vh-50 pt-7 pb-9 m-3 border-radius-lg"
+            style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-cover.jpg');">
+            <span class="mask bg-gradient-dark opacity-6"></span>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 text-center mx-auto">
+                        <ul style="list-style: none;">
+                            <li class="text-white text-sm"><strong>admin@argon.com</strong> with password <strong>secret</strong></li>
+                            <li class="text-white text-sm"><strong>creator@argon.com</strong> with password <strong>secret</strong></li>
+                            <li class="text-white text-sm"><strong>member@argon.com</strong> with password <strong>secret</strong></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
+                <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
+                    <div class="card mt-5">
+                        <div class="card-header pb-0 text-start">
+                            <h3 class="font-weight-bolder">Login</h3>
+                            <p class="mb-0">Ingresa tu usario y contraseña</p>
+                        </div>
+                        <div class="card-body">
+                            <form role="form" method="POST" action="{{route('login.perform') }}" class="text-start">
+                                @csrf
+                                <label>Email</label>
+                                <div class="mb-3">
+                                    <input type="email" name="email" value="{{ old('email') ?? 'admin@argon.com' }}" class="form-control" placeholder="Email" aria-label="Email">
+                                    @error('email') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
+                                </div>
+                                <label>Contraseña</label>
+                                <div class="mb-3">
+                                    <input type="password" name="password" value="secret" class="form-control" placeholder="Password" aria-label="Password">
+                                    @error('password') <p class='text-danger text-xs pt-1'> {{ $message }} </p> @enderror
+                                </div>
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
+                                    <label class="form-check-label" for="rememberMe">Recordarme</label>
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary w-100 mt-4 mb-0">Iniciar sesión</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                            <p class="mb-1 text-sm mx-auto">
+                                Olvidaste contraseña , podemos reestablecerla
+                                <a href="{{ route('reset-password') }}" class="text-primary text-gradient font-weight-bold">Aquí</a>
+                            </p>
+                        </div>
+                        <div class="card-footer text-center pt-0 px-lg-2 px-1">
+                            <p class="mb-4 text-sm mx-auto">
+                                No tengo cuenta
+                                <a href="{{ route('register') }}" class="text-primary font-weight-bold">Registrate</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    @include('layouts.footers.auth.desc-footer')
+@endsection
---
+++ resources/views/layouts/navbars/guest/topnav.blade.php
@@ -0,0 +1,31 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg position-absolute {{ $classes }}">
+    <div class="{{ $container ?? 'container-fluid'}} ps-2 pe-0">
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon mt-2">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+            </span>
+        </button>
+        <div class="custom-nav-login collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
+            <ul class="navbar-nav navbar-nav-hover mx-auto">
+                <li class="nav-item dropdown dropdown-hover mx-2">
+                    <a href="{{ route('register') }}"
+                        class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center">
+                        Sign Up
+                    </a>
+                </li>
+                <li class="nav-item dropdown dropdown-hover mx-2">
+                    <a href="{{ route('login') }}"
+                        class="nav-link ps-2 d-flex justify-content-between cursor-pointer align-items-center">
+                        Sign In
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Navbar -->
